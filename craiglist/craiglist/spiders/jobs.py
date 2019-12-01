9d2055f3bdfdf58b33b3d25aff26a2394e472696
komodikus <marbel96@yandex.ru>
2019-12-01 12:46:05 +0300

    Added craiglist parser

--- craiglist/craiglist/spiders/jobs.py
+++ craiglist/craiglist/spiders/jobs.py
@@ -15,14 +15,35 @@ class JobsSpider(scrapy.Spider):
             link = listing.xpath('.//a[@class="result-title hdrlnk"]/@href').extract_first()
             text = listing.xpath('.//a[@class="result-title hdrlnk"]/text()').extract_first()
 
-            yield {
-                'date': date,
-                'link': link,
-                'text': text
-            }
+            yield scrapy.Request(link, callback=self.parse_listing,
+                                 meta={
+                                     'date': date,
+                                     'link': link,
+                                     'text': text
+                                 })
         next_page_url = response.xpath('//*[@class="button next"]/@href').extract_first()
-        print('*'*20)
-        print(next_page_url)
-        print('*'*20)
+
         if next_page_url:
             yield scrapy.Request(response.urljoin(next_page_url), callback=self.parse)
+
+    def parse_listing(self, response):
+        date = response.meta['date'],
+        link = response.meta['link']
+        text = response.meta['text']
+
+        compensation = response.xpath('//*[@class="attrgroup"]/span[1]/b/text()').extract_first()
+        job_type = response.xpath('//*[@class="attrgroup"]/span[2]/b/text()').extract_first()
+
+        images = response.xpath('//*[@id="thumbs"]//@src').extract()
+        images = [image.replace("50x50c", "600x450") for image in images]
+
+        job_desc = response.xpath('//*[@id="postingbody"]/text()').extract()
+        yield {
+            'date': date,
+            'link': link,
+            'text': text,
+            'compensation': compensation,
+            'job_type': job_type,
+            'images': images,
+            'job_desc': job_desc,
+        }
